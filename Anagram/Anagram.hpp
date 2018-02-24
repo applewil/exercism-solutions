c878e2f471052d22d7d40e9a562102dc70430be3
applewil <applewil@users.noreply.github.com>
2018-02-24 17:13:32 -0500

    added Anagram

---
+++ Anagram/Anagram.hpp
@@ -0,0 +1,45 @@
+#ifndef ANAGRAM_HPP
+#define ANAGRAM_HPP
+
+class Anagram {
+private:
+    static const size_t CHAR_SET_LENGTH = CHAR_MAX - CHAR_MIN + 1;
+    static const char TO_LOWERCASE_BIT_PATTERN = 0x20;
+    std::string subject;
+    std::vector<std::string> _matches;
+    static bool match(std::string a, std::string b) {
+        int a_counts[CHAR_SET_LENGTH] = {0};
+        int b_counts[CHAR_SET_LENGTH] = {0};
+        char a_char;
+        char b_char;
+        bool equal = true;
+        size_t i;
+
+        if (a.length() != b.length()) return false;
+        for (i = 0; i < a.length(); i++) {
+            a_char = a[i] | TO_LOWERCASE_BIT_PATTERN;
+            b_char = b[i] | TO_LOWERCASE_BIT_PATTERN;
+            a_counts[(int) a_char]++;
+            b_counts[(int) b_char]++;
+            if (a_char != b_char) equal = false;
+        }
+        if (equal) return false;
+        for (i = 0; i < CHAR_SET_LENGTH; i++) {
+            if (a_counts[i] != b_counts[i]) return false;
+        }
+        return true;
+    }
+
+public:
+    Anagram(std::string subject) : subject(subject) {}
+    std::vector<std::string>& matches(std::vector<std::string> candidates) {
+        std::vector<std::string>::iterator it;
+
+        for (it = candidates.begin(); it != candidates.end(); ++it) {
+            if (match(subject, *it)) _matches.push_back(*it);
+        }
+        return _matches;
+    }
+};
+
+#endif
---
+++ Anagram/test.cpp
@@ -0,0 +1,90 @@
+// clang-format off
+#include <climits>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include "Anagram.hpp"
+
+#define BOOST_TEST_MODULE Anagram
+#include <boost/test/included/unit_test.hpp>
+// clang-format on
+
+BOOST_AUTO_TEST_CASE(no_matches) {
+    Anagram anagram("diaper");
+    auto matches = anagram.matches({"hello", "world", "zombies", "pants"});
+    std::vector<std::string> expected;
+
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
+}
+
+BOOST_AUTO_TEST_CASE(detects_simple_anagram) {
+    Anagram anagram("ant");
+    auto matches = anagram.matches({"tan", "stand", "at"});
+    std::vector<std::string> expected{"tan"};
+
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
+}
+
+BOOST_AUTO_TEST_CASE(does_not_detect_false_positives) {
+    Anagram anagram("galea");
+    auto matches = anagram.matches({"eagle"});
+    std::vector<std::string> expected;
+
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
+}
+
+BOOST_AUTO_TEST_CASE(detects_multiple_anagrams) {
+    Anagram anagram("master");
+    auto matches = anagram.matches({"stream", "pigeon", "maters"});
+    std::vector<std::string> expected{"stream", "maters"};
+
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
+}
+
+BOOST_AUTO_TEST_CASE(does_not_detect_anagram_subsets) {
+    Anagram anagram("good");
+    auto matches = anagram.matches({"dog", "goody"});
+    std::vector<std::string> expected;
+
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
+}
+
+BOOST_AUTO_TEST_CASE(detects_anagram) {
+    Anagram anagram("listen");
+    auto matches = anagram.matches({"enlists", "google", "inlets", "banana"});
+    std::vector<std::string> expected{"inlets"};
+
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
+}
+
+BOOST_AUTO_TEST_CASE(detects_multiple_anagrams2) {
+    Anagram anagram("allergy");
+    auto matches = anagram.matches({"gallery", "ballerina", "regally", "clergy", "largely", "leading"});
+    std::vector<std::string> expected{"gallery", "regally", "largely"};
+
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
+}
+
+BOOST_AUTO_TEST_CASE(detects_anagrams_case_insensitively) {
+    Anagram anagram("Orchestra");
+    auto matches = anagram.matches({"cashregister", "Carthorse", "radishes"});
+    std::vector<std::string> expected{"Carthorse"};
+
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
+}
+
+BOOST_AUTO_TEST_CASE(does_not_detect_a_word_as_its_own_anagram) {
+    Anagram anagram("banana");
+    auto matches = anagram.matches({"Banana"});
+    std::vector<std::string> expected;
+
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
+}
+
+BOOST_AUTO_TEST_CASE(matches_accepts_std_string_arguments) {
+    Anagram anagram("ant");
+    auto matches = anagram.matches({"stand", "tan", "at"});
+    std::vector<std::string> expected{"tan"};
+
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(expected.begin(), expected.end(), matches.begin(), matches.end());
+}
