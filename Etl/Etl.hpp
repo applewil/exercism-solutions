db2e519916063ae2bbd234000e12d64eefefa1d5
applewil <applewil@users.noreply.github.com>
2018-03-04 18:03:38 -0500

    added sequential etl

--- Etl/Etl.hpp
+++ Etl/Etl.hpp
@@ -10,6 +10,14 @@ private:
 public:
     Etl(const std::unordered_map<K, std::vector<V>> subject) : subject(subject) {}
     const std::unordered_map<V, K>& transform() {
+        typename std::unordered_map<K, std::vector<V>>::const_iterator i;
+        typename std::vector<V>::const_iterator j;
+
+        for (i = subject.cbegin(); i != subject.cend(); i++) {
+            for (j = i->second.cbegin(); j != i->second.cend(); j++) {
+                transformed.insert({tolower(*j), i->first});
+            }
+        }
         return transformed;
     }
 };
